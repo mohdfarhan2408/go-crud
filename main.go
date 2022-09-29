cb6c90dc755e30e8524c5be6f6e9fc8b2554f632
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-09-29 13:57:22 +0800

    add ,env file

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	
+)
+
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+  }
+}
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	r.Run() 
 }
