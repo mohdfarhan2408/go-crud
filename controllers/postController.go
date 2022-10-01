fede6ad9a7e360ffb9396f59247f8498e47ece18
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-10-01 17:04:58 +0800

    finishing api

--- controllers/postController.go
+++ controllers/postController.go
@@ -11,10 +11,15 @@ import (
 func PostsCreate (c *gin.Context) {
 
 	//Get data of req body
+	var body struct {
+		Body string
+		Title string
+	}
 
+	c.Bind(&body)
 
 	//Create a post
-	post := models.Post{Title: "Jinzhu", Body : "post"}
+	post := models.Post{Title: body.Title, Body : body.Body}
 
 	result := initializers.DB.Create(&post)
 
@@ -27,4 +32,74 @@ func PostsCreate (c *gin.Context) {
 	c.JSON(200, gin.H{
 		"post": post,
 	})
+}
+
+func PostsIndex (c *gin.Context) {
+
+	//Get the posts
+	var posts []models.Post //array
+	initializers.DB.Find(&posts)
+
+	//Respond with them
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostsShow (c *gin.Context) {
+	//Get id of url
+	id := c.Param("id")
+
+	//Get the post
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	//Respond with them
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsUpdate (c *gin.Context) {
+	//Get the id of the url
+	id := c.Param("id")
+
+	//Get the data off req body
+	var body struct {
+		Body string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	//Find the post were updating
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+
+	//Update it
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body: body.Body,
+	})
+
+	//Respond it
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+
+}
+
+func PostsDelete (c *gin.Context) {
+	//Get the id  of the url
+	id := c.Param("id")
+
+	//Delete the data in db
+	initializers.DB.Delete(&models.Post{}, id)
+
+	//Respond
+	c.Status(200, gin.H{
+		"status" : "success",
+	})
+
 }
--- main.go
+++ main.go
@@ -21,6 +21,12 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/posts", controllers.PostsCreate)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 
 	r.Run() 
 }
